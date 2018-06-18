ebca25d5fc9603abe42b23bd9c1fc721d8c0f977
Aliaksandr Kananenka <Aliaksandr_Kananenka@epam.com>
2018-06-18 22:32:58 +0300

    Add check input parameters in RoleManagerController and attribute.

--- Day1.SOLID.HW/Rocket.Web/Controllers/UserRole/RoleManagerController.cs
+++ Day1.SOLID.HW/Rocket.Web/Controllers/UserRole/RoleManagerController.cs
@@ -22,8 +22,11 @@ namespace Rocket.Web.Controllers.UserRole
         [SwaggerResponseRemoveDefaults]
         [SwaggerResponse(HttpStatusCode.NotFound, "Data is not valid", typeof(string))]
         [SwaggerResponse(HttpStatusCode.OK, "Role added to user")]
+        [SwaggerResponse(HttpStatusCode.BadRequest, "Data is not valid")]
         public async Task<IHttpActionResult> AddToRole(string userId, string roleId)
         {
+            if (userId == null || roleId == null)
+                return BadRequest();
             var result = await _roleManager.AddToRole(userId, roleId);
             return result.Succeeded ? (IHttpActionResult)NotFound() : Ok();
         }
@@ -33,8 +36,11 @@ namespace Rocket.Web.Controllers.UserRole
         [SwaggerResponseRemoveDefaults]
         [SwaggerResponse(HttpStatusCode.NotFound, "Data is not valid", typeof(string))]
         [SwaggerResponse(HttpStatusCode.OK, "Role removed from user")]
+        [SwaggerResponse(HttpStatusCode.BadRequest, "Data is not valid")]
         public async Task<IHttpActionResult> RemoveFromRole(string userId, string roleId)
         {
+            if (userId == null || roleId == null)
+                return BadRequest();
             var removeResult = await _roleManager.RemoveFromRole(userId, roleId);
             return removeResult.Succeeded ?  Ok() : (IHttpActionResult)NotFound();
         }
@@ -44,8 +50,11 @@ namespace Rocket.Web.Controllers.UserRole
         [SwaggerResponseRemoveDefaults]
         [SwaggerResponse(HttpStatusCode.NotFound, "Data is not valid", typeof(string))]
         [SwaggerResponse(HttpStatusCode.OK)]
+        [SwaggerResponse(HttpStatusCode.BadRequest, "Data is not valid")]
         public IHttpActionResult GetRoles(string userId)
         {
+            if (userId == null)
+                return BadRequest();
             return _roleManager.GetRoles(userId) == null ? (IHttpActionResult)NotFound() : Ok();
         }
 
@@ -54,8 +63,12 @@ namespace Rocket.Web.Controllers.UserRole
         [SwaggerResponseRemoveDefaults]
         [SwaggerResponse(HttpStatusCode.NotFound, "Data is not valid", typeof(string))]
         [SwaggerResponse(HttpStatusCode.OK)]
+        [SwaggerResponse(HttpStatusCode.BadRequest, "Data is not valid")]
         public async Task<IHttpActionResult> IsInRole(string userId, string roleId)
         {
+            if (userId == null || roleId == null)
+                return BadRequest();
+
             var isInRoleResult = await _roleManager.IsInRole(userId, roleId);
 
             return isInRoleResult ? Ok() : (IHttpActionResult)NotFound();
